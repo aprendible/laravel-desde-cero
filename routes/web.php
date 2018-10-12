5ed3ada04c99545b27b0e991a7dc39780cf0d186
Jorge García <jorge@aprendible.com>
2018-10-12 12:44:41 -0500

    01 - Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// aprendible.com = Route::get('/', function)
+// aprendible.com/contacto = Route::get('contacto', function)
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('saludo/{nombre?}', function($nombre = "Invitado"){
+	return "Saludos " . $nombre;
 });
+
+// Route::get('/', function(){
+// 	return "Hola desde la página de inicio";
+// });
+
+// Route::get('contacto', function(){
+	// return "Hola desde la página de contacto";
+// });
+
+// Route::get()
+// Route::post() // form action="POST"
+// Route::put()
+// Route::patch()
+// Route::delete()
