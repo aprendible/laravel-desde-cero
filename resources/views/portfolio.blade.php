356d3bdbe8cf6b4ddc5b6627d13d3ef273e8e1bd
Jorge García <jorge@aprendible.com>
2018-11-04 20:48:01 -0500

    05 - Estructuras de control con Blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,4 +4,12 @@
 
 @section('content')
     <h1>Portfolio</h1>
+    <ul>
+        @forelse($portfolio as $portfolioItem)
+            <li>{{ $portfolioItem['title'] }}</li>
+        @empty
+            <li>No hay proyectos para mostrar</li>
+        @endforelse
+    </ul>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+$portfolio = [
+    ['title' => 'Proyecto #1'],
+    ['title' => 'Proyecto #2'],
+    ['title' => 'Proyecto #3'],
+    ['title' => 'Proyecto #4'],
+];
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
