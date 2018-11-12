d2f6e37de9bcaef785743cd3b34472089b98be69
Jorge García <jorge@aprendible.com>
2018-11-12 16:22:07 -0500

    06 - Controladores

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $portfolio = [
+            ['title' => 'Proyecto #1'],
+            ['title' => 'Proyecto #2'],
+            ['title' => 'Proyecto #3'],
+            ['title' => 'Proyecto #4'],
+        ];
+
+        return view('portfolio', compact('portfolio'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4'],
-];
-
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
