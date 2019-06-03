d71a830cec0330dba4104d84538d828e15863625
Jorge García <jorge@aprendible.com>
2019-06-03 18:32:30 -0500

    27 - Qué es la asignación masiva

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -28,7 +28,13 @@ class ProjectController extends Controller
 
     public function store()
     {
-        Project::create( request()->all() );
+        $fields = request()->validate([
+            'title' => 'required',
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
+        Project::create($fields);
 
         return redirect()->route('projects.index');
     }
--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'url', 'description'];
+    protected $guarded = [];
 
     public function getRouteKeyName()
     {
