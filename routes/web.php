fa59b9bfca1fa7e7bd3add4c39dc3b809d804cc8
Jorge García <jorge@aprendible.com>
2019-07-08 18:04:12 -0500

    32 - Route resource

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 Route::view('/', 'home')->name('home');
 Route::view('/quienes-somos', 'about')->name('about');
 
-Route::get('/portafolio', 'ProjectController@index')->name('projects.index');
-Route::get('/portafolio/crear', 'ProjectController@create')->name('projects.create');
-Route::get('/portafolio/{project}/editar', 'ProjectController@edit')->name('projects.edit');
-Route::patch('/portafolio/{project}', 'ProjectController@update')->name('projects.update');
-Route::post('/portafolio', 'ProjectController@store')->name('projects.store');
-Route::get('/portafolio/{project}', 'ProjectController@show')->name('projects.show');
-Route::delete('/portafolio/{project}', 'ProjectController@destroy')->name('projects.destroy');
+Route::resource('portafolio', 'ProjectController')
+    ->parameters(['portafolio' => 'project'])
+    ->names('projects');
 
 Route::view('/contacto', 'contact')->name('contact');
 Route::post('contact', 'MessageController@store')->name('messages.store');
