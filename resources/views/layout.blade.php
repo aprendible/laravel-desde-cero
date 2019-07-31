5c49153ec8518db1965d2c0c6c06d830a481cec1
Jorge García <jorge@aprendible.com>
2019-07-31 10:19:32 -0500

    36 - Introducción a Laravel Mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,12 +2,8 @@
 <html>
 <head>
     <title>@yield('title', 'Aprendible')</title>
-    <style>
-        .active a {
-            color: red;
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
     @include('partials.nav')
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync('http://laravel.test');
+
+if (mix.inProduction())
+{
+    mix.version();
+}
