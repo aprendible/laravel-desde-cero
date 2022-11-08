86b85c682ce341921c1c3aa338df81e31c59abaa
juan.tabares <juan.tabares@coytex.com.co>
2022-11-07 23:26:53 -0500

    evitando registros duplicados para la url de los proyectos

--- app/Http/Requests/SaveProjectRequest.php
+++ app/Http/Requests/SaveProjectRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SaveProjectRequest extends FormRequest
@@ -23,9 +24,10 @@ class SaveProjectRequest extends FormRequest
      */
     public function rules()
     {
+        
         return [
             'title' => 'required',
-            'url' => 'required',
+            'url' => ['required', Rule::unique('projects')->ignore($this->route('project'))],
             'description' => 'required',
         ];
     }
